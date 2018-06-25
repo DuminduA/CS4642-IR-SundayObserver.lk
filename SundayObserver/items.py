115020d6eeb7b6c5443613e20690ce27b8d32f41
Akalanka <duminduakalanka@gmail.com>
2018-06-25 19:00:51 +0530

    Identified items

--- SundayObserver/items.py
+++ SundayObserver/items.py
@@ -12,3 +12,32 @@ class SundayobserverItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class FullArticleItem(scrapy.Item):
+    image = scrapy.Field()
+    title = scrapy.Field()
+    title_byline = scrapy.Field()
+    body = scrapy.Field()
+    date = scrapy.Field()
+    comments = scrapy.Field()
+    link = scrapy.Field()
+    author = scrapy.Field()
+
+class SummaryArticleItem(scrapy.Item):
+    image = scrapy.Field()
+    title = scrapy.Field()
+    title_byline = scrapy.Field()
+    body = scrapy.Field()
+    date = scrapy.Field()
+    comments = scrapy.Field()
+    link = scrapy.Field()
+
+class EditorialItem(scrapy.Item):
+    image = scrapy.Field()
+    title = scrapy.Field()
+    title_byline = scrapy.Field()
+    body = scrapy.Field()
+    date = scrapy.Field()
+    comments = scrapy.Field()
+    link = scrapy.Field()
+
